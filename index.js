4f73d27fe49dd51be2576521cc993cde75355c7d
Zabalena <rizabalena20@gmail.com>
2023-01-02 18:17:32 +0700

    backend laman produk, tambahan

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ dataProduk.forEach(function(kt){
   kategoriList.push({"nama": kt["nama_kategori"], "nomor": kt["nomor_kategori"], "img_source": kt["img_source"]})
 })
 
-
-
 function pilihKategoriProduk(kategori) {
   return dataProduk.find(kt => kt.nomor_kategori == kategori)
 }
@@ -19,14 +17,12 @@ app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
 // app.use("/static", express.static(path.resolve('./public/assets')));
 
-
 app.get('/', (req, res) => {
   res.render("beranda", { kategori_list: kategoriList})
   // console.log(dataProduk.find(kt => kt.nama_kategori == "Aksesoris"))
   // console.log(dataProduk)
   // console.log(kategoriList)
 })
-
 
 app.get('/produk', (req, res) => {
   var withParams = Object.keys(req.params).length != 0
@@ -37,24 +33,32 @@ app.get('/produk', (req, res) => {
 
 app.get('/produk/:kategori/', (req, res) => {
   let selectedKategori = pilihKategoriProduk(req.params['kategori'])
-  console.log(kategoriList)
+  // console.log(kategoriList)
   // console.log(req.params)
   let withParams = Object.keys(req.params).length != 0
   // console.log(withParams)
   // console.log(selectedKategori)
-  res.render("produk", { produk_list: selectedKategori["produk_kategori"], withParams, kategoriList })
+  // console.log(selectedKategori["nomor_kategori"])
+  res.render("produk", { ctg_num: selectedKategori["nomor_kategori"] ,produk_list: selectedKategori["produk_kategori"], withParams, kategoriList })
   // console.log(req.params === 0)
 })
 
-app.get('/lihat', (req, res) => {
-  res.render("lihat_produk")
+app.get('/lihat/:kategori/:produk', (req, res) => {
+  dataKategori = dataProduk.find(kt => kt.nomor_kategori == req.params.kategori)
+  // console.log(req.params)
+  dataBarang = dataKategori["produk_kategori"].find(prod => prod.seri_produk == req.params.produk)
+  // console.log(dataBarang)
+  // console.log(dataKategori["produk_kategori"])
+  // console.log(dataKategori["nama_kategori"])
+  res.render("lihat_produk", { dataBarang, "nama_kategori": dataKategori["nama_kategori"], "nomor_kategori": dataKategori["nomor_kategori"] })
 })
 
 app.get('/profil', (req, res) => {
   res.render("profil", {"params": req.params})
 })
 
-app.get('/profil/:cek', (req, res) => {
+app.get('/profil/:cek/:ricek', (req, res) => {
+  console.log(req.params)
   res.render("profil", {"params": req.params})
 })
 
