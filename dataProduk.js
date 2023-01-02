8c949eded37f56f56213d139faf29c3a7c8aef5f
Zabalena <rizabalena20@gmail.com>
2023-01-02 13:01:16 +0700

    file dengan data produk

---
+++ dataProduk.js
@@ -0,0 +1,68 @@
+const dataProduk = [
+  {
+    "nomor_kategori": 1,
+    "nama_kategori": "Aksesoris",
+    "img_source": "/images/produk/meteran-air.png",
+    "produk_kategori": [
+      { "nama_produk": "Meteran Air", "img_source": "/images/meteran-air.png" },
+      { "nama_produk": "Pelampung Radar Daiton", "img_source": "/images/produk/pelampung_radar_daiton.png" },
+      { "nama_produk": "Keran Dispenser", "img_source": "/images/produk/keran_dispenser.jfif" },
+      { "nama_produk": "Pelampung Bola", "img_source": "/images/produk/pelampung_bola.png" },
+      { "nama_produk": "Pelampung Bola Stainless", "img_source": "/images/produk/pelampung_bola_stainless.jfif" },
+      { "nama_produk": "Kran Pelampung Bola", "img_source": "/images/produk/kran_pelampung_bola.jfif" },
+      { "nama_produk": "Elektrolisa", "img_source": "/images/produk/elektrolisa.jfif" }
+    ]
+  },
+  {
+    "nomor_kategori": 2,
+    "nama_kategori": "Booster Pump RO",
+    "img_source": "/images/produk/booster_pump_micron_24.jfif",
+    "produk_kategori": [
+      { "nama_produk": "Booster Pump Micron 24 Volt", "img_source": "/images/produk/booster_pump_micron_24.jfif" },
+      { "nama_produk": "Booster Pump KO JINE 48 Volt", "img_source": "/images/produk/booster_pump_ko_jine_48.jfif" },
+      { "nama_produk": "Booster Pump Kemplo 48 Volt", "img_source": "/images/produk/booster_pump_kemplo_48.jfif" },
+    ]
+  },
+  {
+    "nomor_kategori": 3,
+    "nama_kategori": "Carbon Activited",
+    "img_source": "/images/produk/carbon-cropped.png",
+    "produk_kategori": [
+      { "nama_produk": "Ruby Carb 800", "img_source": "/images/produk/carbon-cropped.png" }
+    ]
+  },
+  {
+    "nomor_kategori": 4,
+    "nama_kategori": "Filter Air",
+    "img_source": "/images/produk/resin_cation_10.png",
+    "produk_kategori": [
+      { "nama_produk": "Resin Cation 10", "img_source": "/images/produk/resin_cation_10.png" },
+      { "nama_produk": "Bio Energi", "img_source": "/images/produk/bio_energi.png" },
+      { "nama_produk": "Bio Keramik", "img_source": "/images/produk/bio_keramik.png" },
+      { "nama_produk": "Post Carbon", "img_source": "/images/produk/post_carbon.jfif" },
+    ]
+  },
+  {
+    "nomor_kategori": 5,
+    "nama_kategori": "Galon Air",
+    "img_source": "/images/produk/galon_guci_12_liter.png",
+    "produk_kategori": [
+      { "nama_produk": "Galon Guci 12 Liter", "img_source": "/images/produk/galon_guci_12_liter.png" },
+      { "nama_produk": "Mesin Sikat Galon", "img_source": "/images/produk/mesin_sikat_galon.jfif" },
+      { "nama_produk": "Botol 330 ml", "img_source": "/images/produk/botol_330_ml.jfif" },
+      { "nama_produk": "Botol 600 ml", "img_source": "/images/produk/botol_600_ml.jfif" }
+    ]
+  },
+  {
+    "nomor_kategori": 6,
+    "nama_kategori": "Housing Filter",
+    "img_source": "/images/produk/housing_star_life_blue_10.jfif",
+    "produk_kategori": [
+      { "nama_produk": "Housing Star Life Blue 20 inch", "img_source": "/images/produk/housing_star_life_blue_10.jfif" },
+      { "nama_produk": "Housing Filter Air 10 inch Water Wizard", "img_source": "/images/produk/housing_filter_air_10_water_wizard.jfif" },
+      { "nama_produk": "Housing Filter Star Life Clear 10 inch", "img_source": "/images/produk/housing_filter_star_life_clear_10.jfif" }
+    ]
+  }
+];
+
+module.exports = dataProduk
--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path')
+
+const dataProduk = require("./dataProduk")
+let kategoriList = [] 
+dataProduk.forEach(function(kt){
+  kategoriList.push({"nama": kt["nama_kategori"], "img_source": kt["img_source"]})
+})
 
 const kategori_list = [
   { "nomor": 1, "nama": "AKSESORIS", "img_source": "images/produk/meteran-air.png"},
@@ -12,26 +19,37 @@ const kategori_list = [
 ]
 
 const produk_list = [
-  {"nomor": 1, "nama_produk": "Tutup Gunung", "img_source": "images/tutup_gunung.jfif"},
-  {"nomor": 2, "nama_produk": "Meteran Air", "img_source": "images/produk/meteran-air.png"},
-  {"nomor": 3, "nama_produk": "Pelampung Radar Daiton", "img_source": "images/produk/pelampung_radar_daiton.png"},
-  {"nomor": 4, "nama_produk": "Keran Dispenser", "img_source": "images/produk/keran_dispenser.jfif"},
-  {"nomor": 5, "nama_produk": "Pelampung Bola", "img_source": "images/produk/pelampung_bola.png"},
-  {"nomor": 6, "nama_produk": "Pelampung Bola Stainless", "img_source": "images/produk/pelampung_bola_stainless.jfif"},
-  {"nomor": 7, "nama_produk": "Kran Pelampung Bola", "img_source": "images/produk/kran_pelampung_bola.jfif"},
-  {"nomor": 8, "nama_produk": "Elektrolisa", "img_source": "images/produk/elektrolisa.jfif"},
+  {"nomor": 1, "nama_produk": "Tutup Gunung", "img_source": "/images/tutup_gunung.jfif"},
+  {"nomor": 2, "nama_produk": "Meteran Air", "img_source": "/images/produk/meteran-air.png"},
+  {"nomor": 3, "nama_produk": "Pelampung Radar Daiton", "img_source": "/images/produk/pelampung_radar_daiton.png"},
+  {"nomor": 4, "nama_produk": "Keran Dispenser", "img_source": "/images/produk/keran_dispenser.jfif"},
+  {"nomor": 5, "nama_produk": "Pelampung Bola", "img_source": "/images/produk/pelampung_bola.png"},
+  {"nomor": 6, "nama_produk": "Pelampung Bola Stainless", "img_source": "/images/produk/pelampung_bola_stainless.jfif"},
+  {"nomor": 7, "nama_produk": "Kran Pelampung Bola", "img_source": "/images/produk/kran_pelampung_bola.jfif"},
+  {"nomor": 8, "nama_produk": "Elektrolisa", "img_source": "/images/produk/elektrolisa.jfif"},
   
 ]
 
 app.set('view engine', 'ejs')
-app.use(express.static("public"))
+app.use(express.static(path.join(__dirname, 'public')))
+// app.use("/static", express.static(path.resolve('./public/assets')));
+
 
 app.get('/', (req, res) => {
-  res.render("beranda", { kategori_list: kategori_list})
+  res.render("beranda", { kategori_list: kategoriList})
+  // console.log(dataProduk)
+  // console.log(kategoriList)
 })
+
 
 app.get('/produk', (req, res) => {
   res.render("produk", { produk_list: produk_list })
+  console.log(Object.keys(req.params).length == 0)
+})
+
+app.get('/produk/:kategori/', (req, res) => {
+  res.render("produk", { produk_list: produk_list })
+  // console.log(req.params === 0)
 })
 
 app.get('/lihat', (req, res) => {
@@ -39,12 +57,16 @@ app.get('/lihat', (req, res) => {
 })
 
 app.get('/profil', (req, res) => {
-  res.send("Profil Perusahaan")
+  res.render("profil", {"params": req.params})
 })
 
-app.get("/produk", (req, res) => {
-  res.send("Berikut kategori produk")
+app.get('/profil/:cek', (req, res) => {
+  res.render("profil", {"params": req.params})
 })
+
+// app.get("/produk", (req, res) => {
+//   res.send("Berikut kategori produk")
+// })
 
 app.get("/blog", (req, res) => {
   res.send("Berita seputar water treatment")
