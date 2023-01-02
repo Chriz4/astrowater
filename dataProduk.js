34b4e92b557ab2d31cdbc2263ba548d2a4f7877f
Zabalena <rizabalena20@gmail.com>
2023-01-02 15:59:39 +0700

    backend data laman utama dan kategori

--- dataProduk.js
+++ dataProduk.js
@@ -1,16 +1,16 @@
 const dataProduk = [
   {
     "nomor_kategori": 1,
-    "nama_kategori": "Aksesoris",
+    "nama_kategori": "aksesoris",
     "img_source": "/images/produk/meteran-air.png",
     "produk_kategori": [
-      { "nama_produk": "Meteran Air", "img_source": "/images/meteran-air.png" },
-      { "nama_produk": "Pelampung Radar Daiton", "img_source": "/images/produk/pelampung_radar_daiton.png" },
-      { "nama_produk": "Keran Dispenser", "img_source": "/images/produk/keran_dispenser.jfif" },
-      { "nama_produk": "Pelampung Bola", "img_source": "/images/produk/pelampung_bola.png" },
-      { "nama_produk": "Pelampung Bola Stainless", "img_source": "/images/produk/pelampung_bola_stainless.jfif" },
-      { "nama_produk": "Kran Pelampung Bola", "img_source": "/images/produk/kran_pelampung_bola.jfif" },
-      { "nama_produk": "Elektrolisa", "img_source": "/images/produk/elektrolisa.jfif" }
+      { "seri_produk": "01001",  "nama_produk": "Meteran Air", "img_source": "/images/produk/meteran-air.png" },
+      { "seri_produk": "01002",  "nama_produk": "Pelampung Radar Daiton", "img_source": "/images/produk/pelampung_radar_daiton.png" },
+      { "seri_produk": "01003",  "nama_produk": "Keran Dispenser", "img_source": "/images/produk/keran_dispenser.jfif" },
+      { "seri_produk": "01004",  "nama_produk": "Pelampung Bola", "img_source": "/images/produk/pelampung_bola.png" },
+      { "seri_produk": "01005",  "nama_produk": "Pelampung Bola Stainless", "img_source": "/images/produk/pelampung_bola_stainless.jfif" },
+      { "seri_produk": "01006",  "nama_produk": "Kran Pelampung Bola", "img_source": "/images/produk/kran_pelampung_bola.jfif" },
+      { "seri_produk": "01007",  "nama_produk": "Elektrolisa", "img_source": "/images/produk/elektrolisa.jfif" }
     ]
   },
   {
@@ -18,9 +18,9 @@ const dataProduk = [
     "nama_kategori": "Booster Pump RO",
     "img_source": "/images/produk/booster_pump_micron_24.jfif",
     "produk_kategori": [
-      { "nama_produk": "Booster Pump Micron 24 Volt", "img_source": "/images/produk/booster_pump_micron_24.jfif" },
-      { "nama_produk": "Booster Pump KO JINE 48 Volt", "img_source": "/images/produk/booster_pump_ko_jine_48.jfif" },
-      { "nama_produk": "Booster Pump Kemplo 48 Volt", "img_source": "/images/produk/booster_pump_kemplo_48.jfif" },
+      { "seri_produk": "02001",  "nama_produk": "Booster Pump Micron 24 Volt", "img_source": "/images/produk/booster_pump_micron_24.jfif" },
+      { "seri_produk": "02002",  "nama_produk": "Booster Pump KO JINE 48 Volt", "img_source": "/images/produk/booster_pump_ko_jine_48.jfif" },
+      { "seri_produk": "02003",  "nama_produk": "Booster Pump Kemplo 48 Volt", "img_source": "/images/produk/booster_pump_kemplo_48.jfif" },
     ]
   },
   {
@@ -28,7 +28,7 @@ const dataProduk = [
     "nama_kategori": "Carbon Activited",
     "img_source": "/images/produk/carbon-cropped.png",
     "produk_kategori": [
-      { "nama_produk": "Ruby Carb 800", "img_source": "/images/produk/carbon-cropped.png" }
+      { "seri_produk": "03001", "nama_produk": "Ruby Carb 800", "img_source": "/images/produk/carbon-cropped.png" }
     ]
   },
   {
@@ -36,10 +36,10 @@ const dataProduk = [
     "nama_kategori": "Filter Air",
     "img_source": "/images/produk/resin_cation_10.png",
     "produk_kategori": [
-      { "nama_produk": "Resin Cation 10", "img_source": "/images/produk/resin_cation_10.png" },
-      { "nama_produk": "Bio Energi", "img_source": "/images/produk/bio_energi.png" },
-      { "nama_produk": "Bio Keramik", "img_source": "/images/produk/bio_keramik.png" },
-      { "nama_produk": "Post Carbon", "img_source": "/images/produk/post_carbon.jfif" },
+      { "seri_produk": "04001", "nama_produk": "Resin Cation 10", "img_source": "/images/produk/resin_cation_10.png" },
+      { "seri_produk": "04002", "nama_produk": "Bio Energi", "img_source": "/images/produk/bio_energi.png" },
+      { "seri_produk": "04003", "nama_produk": "Bio Keramik", "img_source": "/images/produk/bio_keramik.png" },
+      { "seri_produk": "04004", "nama_produk": "Post Carbon", "img_source": "/images/produk/post_carbon.jfif" },
     ]
   },
   {
@@ -47,10 +47,10 @@ const dataProduk = [
     "nama_kategori": "Galon Air",
     "img_source": "/images/produk/galon_guci_12_liter.png",
     "produk_kategori": [
-      { "nama_produk": "Galon Guci 12 Liter", "img_source": "/images/produk/galon_guci_12_liter.png" },
-      { "nama_produk": "Mesin Sikat Galon", "img_source": "/images/produk/mesin_sikat_galon.jfif" },
-      { "nama_produk": "Botol 330 ml", "img_source": "/images/produk/botol_330_ml.jfif" },
-      { "nama_produk": "Botol 600 ml", "img_source": "/images/produk/botol_600_ml.jfif" }
+      { "seri_produk": "05001", "nama_produk": "Galon Guci 12 Liter", "img_source": "/images/produk/galon_guci_12_liter.png" },
+      { "seri_produk": "05002", "nama_produk": "Mesin Sikat Galon", "img_source": "/images/produk/mesin_sikat_galon.jfif" },
+      { "seri_produk": "05003", "nama_produk": "Botol 330 ml", "img_source": "/images/produk/botol_330_ml.jfif" },
+      { "seri_produk": "05004", "nama_produk": "Botol 600 ml", "img_source": "/images/produk/botol_600_ml.jfif" }
     ]
   },
   {
@@ -58,9 +58,9 @@ const dataProduk = [
     "nama_kategori": "Housing Filter",
     "img_source": "/images/produk/housing_star_life_blue_10.jfif",
     "produk_kategori": [
-      { "nama_produk": "Housing Star Life Blue 20 inch", "img_source": "/images/produk/housing_star_life_blue_10.jfif" },
-      { "nama_produk": "Housing Filter Air 10 inch Water Wizard", "img_source": "/images/produk/housing_filter_air_10_water_wizard.jfif" },
-      { "nama_produk": "Housing Filter Star Life Clear 10 inch", "img_source": "/images/produk/housing_filter_star_life_clear_10.jfif" }
+      { "seri_produk": "06001", "nama_produk": "Housing Star Life Blue 20 inch", "img_source": "/images/produk/housing_star_life_blue_10.jfif" },
+      { "seri_produk": "06002", "nama_produk": "Housing Filter Air 10 inch Water Wizard", "img_source": "/images/produk/housing_filter_air_10_water_wizard.jfif" },
+      { "seri_produk": "06003", "nama_produk": "Housing Filter Star Life Clear 10 inch", "img_source": "/images/produk/housing_filter_star_life_clear_10.jfif" }
     ]
   }
 ];
--- index.js
+++ index.js
@@ -6,29 +6,14 @@ const path = require('path')
 const dataProduk = require("./dataProduk")
 let kategoriList = [] 
 dataProduk.forEach(function(kt){
-  kategoriList.push({"nama": kt["nama_kategori"], "img_source": kt["img_source"]})
+  kategoriList.push({"nama": kt["nama_kategori"], "nomor": kt["nomor_kategori"], "img_source": kt["img_source"]})
 })
 
-const kategori_list = [
-  { "nomor": 1, "nama": "AKSESORIS", "img_source": "images/produk/meteran-air.png"},
-  { "nomor": 2, "nama": "BOOSTER PUMP", "img_source": "images/produk/booster-pump.jfif"},
-  { "nomor": 3, "nama": "CARBON", "img_source": "images/produk/carbon-cropped.png"},
-  { "nomor": 4, "nama": "FILTER AIR", "img_source": "images/produk/filter-air.jfif"},
-  { "nomor": 5, "nama": "HOUSING FILTER", "img_source": "images/produk/housing-filter.jfif"},
-  { "nomor": 6, "nama": "GALON AIR", "img_source": "images/produk/galon-air.png"}
-]
 
-const produk_list = [
-  {"nomor": 1, "nama_produk": "Tutup Gunung", "img_source": "/images/tutup_gunung.jfif"},
-  {"nomor": 2, "nama_produk": "Meteran Air", "img_source": "/images/produk/meteran-air.png"},
-  {"nomor": 3, "nama_produk": "Pelampung Radar Daiton", "img_source": "/images/produk/pelampung_radar_daiton.png"},
-  {"nomor": 4, "nama_produk": "Keran Dispenser", "img_source": "/images/produk/keran_dispenser.jfif"},
-  {"nomor": 5, "nama_produk": "Pelampung Bola", "img_source": "/images/produk/pelampung_bola.png"},
-  {"nomor": 6, "nama_produk": "Pelampung Bola Stainless", "img_source": "/images/produk/pelampung_bola_stainless.jfif"},
-  {"nomor": 7, "nama_produk": "Kran Pelampung Bola", "img_source": "/images/produk/kran_pelampung_bola.jfif"},
-  {"nomor": 8, "nama_produk": "Elektrolisa", "img_source": "/images/produk/elektrolisa.jfif"},
-  
-]
+
+function pilihKategoriProduk(kategori) {
+  return dataProduk.find(kt => kt.nomor_kategori == kategori)
+}
 
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
@@ -37,18 +22,27 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
   res.render("beranda", { kategori_list: kategoriList})
+  // console.log(dataProduk.find(kt => kt.nama_kategori == "Aksesoris"))
   // console.log(dataProduk)
   // console.log(kategoriList)
 })
 
 
 app.get('/produk', (req, res) => {
-  res.render("produk", { produk_list: produk_list })
-  console.log(Object.keys(req.params).length == 0)
+  var withParams = Object.keys(req.params).length != 0
+  console.log(withParams)
+  res.render("produk", { withParams, kategoriList  })
+  // console.log(Object.keys(req.params).length == 0)
 })
 
 app.get('/produk/:kategori/', (req, res) => {
-  res.render("produk", { produk_list: produk_list })
+  let selectedKategori = pilihKategoriProduk(req.params['kategori'])
+  console.log(kategoriList)
+  // console.log(req.params)
+  let withParams = Object.keys(req.params).length != 0
+  // console.log(withParams)
+  // console.log(selectedKategori)
+  res.render("produk", { produk_list: selectedKategori["produk_kategori"], withParams, kategoriList })
   // console.log(req.params === 0)
 })
 
